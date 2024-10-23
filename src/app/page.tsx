a5161a85f27b178708c54240e50dd20f8838b616
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-10-23 16:11:35 +0530

    Landing page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@/components/ui/button"
+import { Poppins } from "next/font/google"
+import { cn } from "@/lib/utils";//for  combining class names
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+})
+export default async function Home() {
 
-
-export default function Home() {
   return (
-    <div className="max-w-screen-xl min-h-screen mx-auto  flex flex-col items-center justify-center">
-      <h1 className=" bg-red-200 font-semibold capitalize p-2 rounded-md mt-2" > hey lets learn auth in next.js </h1>
-      <Button className=" mt-2" variant="default">Lets go</Button>
-
-    </div>
+    <main className="flex flex-col min-h-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-pink-500
+">
+      <h1 className={cn(
+        "text-7xl font-semibold text-white drop-shadow-md",
+        font.className
+      )}>🔐Auth</h1>
+      <p className="text-xl font-medium capitalize text-white  "> 
+       A simple Auth service</p>
+       <Button size='lg'  className=" mt-2 bg-slate-50 text-black  outline-none font-serif font-medium text-lg " > Sign-in</Button>
+    </main>
   );
 }
+//bg-gradient-to-r from-cyan-400 to-blue-600 bg-clip-text text-transparent purple to pink
