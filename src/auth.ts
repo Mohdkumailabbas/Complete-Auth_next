cd50b21aca4a64ec30015cb9ee80254b78d57146
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-10-28 19:39:43 +0530

    id sent from token to session

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,22 @@ import { db } from "./lib/db";
 import { authConfig } from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks:{
+    // Callbacks are asynchronous functions you can use to control what happens when an action is performed.
+   async session({token,session}){
+    console.log({sessionToken:token, session})
+    if(token.sub && session.user){
+      session.user.id=token.sub//sendin token sub(id) in to session
+    }
+    return session
+
+   },
+    async jwt({token}){
+    console.log({token})
+    token.customFiled="test"
+    return token
+   } 
+  },
   adapter: PrismaAdapter(db),
   session:{strategy:"jwt"},
   ...authConfig,//providers are defined in auth-config
