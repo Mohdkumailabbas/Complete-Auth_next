b62d072574331f6714c1fe64ae2145375dd02229
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-10-29 13:59:59 +0530

    oauth ID & Secret Added

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,12 +1,21 @@
 import Credentials from 'next-auth/providers/credentials'; 
 import { LoginSchema } from './schemas';
 import bcrypt from 'bcryptjs';
- // import GitHub from "next-auth/providers/github"
+ import GitHub from "next-auth/providers/github"
+ import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
 import { getUserByEmail } from '@/app/auth/data/user';
  
 export const authConfig:NextAuthConfig=
  { providers: [ 
+      GitHub({
+        clientId:process.env.GITHUB_CLIENT_ID,
+        clientSecret:process.env.GITHUB_CLIENT_SECRET
+      }),
+      Google({
+        clientId:process.env.GOOGLE_CLIENT_ID,
+        clientSecret:process.env.GOOGLE_CLIENT_SECRET
+      }),
     Credentials({
         async authorize(credentials){
             const validatedfields=LoginSchema.safeParse(credentials)
