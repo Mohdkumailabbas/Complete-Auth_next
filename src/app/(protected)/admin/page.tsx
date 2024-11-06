e2529e411366e36171b5c18fc75b4aab922ea976
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-11-06 13:47:39 +0530

    Set up admin module

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,10 +1,57 @@
 "use client"
- 
-const Page=()=>{
+
+import RoleGate from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-succes";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
+ const Page=()=>{
+  const onApiRouteClick=()=>{
+   fetch("/api/admin")
+    .then((response)=>{
+      if(response.ok){
+        toast.success("Access granted!")
+      }
+      else{
+        toast.error("Oops! You’re not authorized to access this API")
+      }
+    })
+  }
     return(
         <div>
-          hey
+          <Card className="mt-4 w-[550px] shadow-lg">
+            <CardHeader>
+              🗝️Admin
+            </CardHeader>
+            <CardContent>
+              <RoleGate allowedRole={UserRole.ADMIN}>
+                <FormSuccess message="Hey Admin! You are allowed to view this content"/>
+              </RoleGate>
+              <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md mt-2">
+               <p className="text-sm font-medium">
+                Admin only Api Route
+               </p>
+               <Button onClick={onApiRouteClick}>
+                Click to Test
+               </Button>
+              </div>
+              <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md mt-2">
+               <p className="text-sm font-medium">
+                Admin only Server Action
+               </p>
+               <Button>
+                Click to Test
+               </Button>
+              </div>
+            </CardContent>
+          </Card>
+        
         </div>
     )
 }
 export default Page;
+
+
+// const role =CurrentRole() for server side
+    // const role =useCurrentRole() for clinet
